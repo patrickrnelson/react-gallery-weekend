3ea42454ace2be64c8925237772e3ee5b28ca055
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-18 18:44:36 -0600

    imported images to the images folder

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,24 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, 
+      path: 'images/north_woods.jpg', 
+      description: 'Photo of a goat taken at Glacier National Park.', 
+      likes: 0 }
+    { id: 2, 
+      path: 'images/boom_island.jpg', 
+      description: '', 
+      likes: 0 }
+    { id: 3, 
+      path: 'images/nelson.jpg', 
+      description: '', 
+      likes: 0 }
+    { id: 4, 
+      path: 'images/mill_city.jpg', 
+      description: '', 
+      likes: 0 }
+    { id: 5, 
+      path: 'images/best_girl.jpg', 
+      description: '', 
+      likes: 0 }
 ];
 
 module.exports = galleryItems;
