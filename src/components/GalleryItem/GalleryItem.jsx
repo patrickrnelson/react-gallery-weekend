3d80fc7352101dd4ebc71d43ba2afe0387b8a042
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-19 20:52:21 -0600

    Only one description shows on an image click

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -11,15 +11,30 @@ function GalleryItem({imageArray, updateLikeCount}) {
   // }
 
   const [showDescription, setShowDescription] = useState(false)
+  const [thisImageDesc, setThisImageDesc] = useState('')
+
+  {/* onClick will.. 
+      setImageDesc to the image desc that's being clicked 
+      & setShowDescription to the opposite of what it is*/}
+  function handleOnClick(desc) {
+    setThisImageDesc(desc);
+    setShowDescription(prevCheck => !prevCheck);
+  }
 
   return (
     <>
     <h3>Gallery List</h3>
       {imageArray.map(image => 
         (<div className="imageContainer" key={image.id}>
-          {/* {descriptionBox} */}
-          <img onClick={() => setShowDescription(prevCheck => !prevCheck)} src={image.path} alt={image.description} width="65%"/>
-          <h4>{showDescription ? image.description : ""}</h4>
+          <img onClick={() => handleOnClick(image.description)} src={image.path} alt={image.description} width="65%"/>
+          
+          {/* IF thisImageDesc == the image desc of the pic being clicked
+          AND IF showDescription is true
+          then show the description of the picture.
+          This is so all desc's don't show on a click*/}
+          <h4>{ image.description == thisImageDesc 
+                ? showDescription ? image.description : ""
+                : ""}</h4>
           <p>Likes: {image.likes}</p>
           <button onClick={() => updateLikeCount(image.id)}>LIKE</button>
         </div>)
