299394614a11db40df84a1e384f139e7129ed3c2
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-19 18:42:33 -0600

    styling and trying to figure out conditional rendering

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,11 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
 function GalleryItem({imageArray, updateLikeCount}) {
+
+  const [descriptionBox, setDescriptionBox] = useState('')
+  
+  const showDescription = (desc) => {
+    console.log('description', desc);
+    setDescriptionBox(<div className="imageOverlay" ><p>{desc}</p></div>);
+  
+  }
 
   return (
     <>
     <h3>Gallery List</h3>
       {imageArray.map(image => 
-        (<div class="imageContainer" key={image.id}>
-          <img src={image.path} alt={image.description} width="400px"/>
+        (<div className="imageContainer" key={image.id}>
+          {/* {descriptionBox} */}
+          <img onClick={() => showDescription(image.description)} src={image.path} alt={image.description} width="90%"/>
           <p>Likes: {image.likes}</p>
           <button onClick={() => updateLikeCount(image.id)}>LIKE</button>
         </div>)
